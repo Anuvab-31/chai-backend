b0edc5fc5e5b1ad9d58f720678a4babb359ce9c9
Anuvab-31 <anuvabb31@gmail.com>
2024-04-06 12:39:10 +0530

    utils code discussion

--- Readme.md
+++ Readme.md
@@ -2,3 +2,6 @@
 This  is a video series on backend 
 
 - [Model Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+mongoose is help for connect with database.
+When we will use middlewares we will mention .use().
---
+++ src/index.js
@@ -0,0 +1,57 @@
+//connect with database
+// require('dotenv').config
+
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+const app = express();
+
+
+connectDB().then(() => {
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+    });
+
+}).catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+//approach 1
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on("error", (error) => {
+//             console.log("ERRR: ", error);
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is Listening on port ${process.env.PORT}`);
+//         })
+
+//     }
+//     catch (error) {
+//         console.error("ERROR: ", error);
+//     }
+// })();
